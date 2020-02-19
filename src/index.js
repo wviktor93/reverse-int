bc03cb59bbe8f42273031fe12cf877dfd37883c9
Victoria Triputina <triputina.victoria@gmail.com>
2020-02-19 23:48:54 +0300

    feat: solve reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0){
+        n = -n;
+    }
+    n = String(n);
+    n = Number(n.split('').reverse().join(''));
+    return n;
 }
